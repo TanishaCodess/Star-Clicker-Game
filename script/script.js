a1c670c0723bd0fe1e392504be34eb3d10d87dae
Lezgo7 <tanishaminson@gmail.com>
2020-09-10 18:18:10 -0400

    basic index structure

---
+++ script/script.js
@@ -0,0 +1,18 @@
+const starClickerApp = {};
+
+
+// prompt to ask for name when they visit the page
+// store than name in a variable
+// const name = prompt("Hi! Whats your name?");
+
+
+
+
+
+
+
+
+// Document Ready
+$(document).ready(function() {
+    starClickerApp.init();
+});
