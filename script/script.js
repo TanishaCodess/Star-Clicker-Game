784fedeea6cf00fe8fc980cb163e5a90f81cab3a
Lezgo7 <tanishaminson@gmail.com>
2020-09-12 16:24:19 -0400

    basic js functionality done! sources added

--- script/script.js
+++ script/script.js
@@ -3,13 +3,25 @@ const starApp = {};
 
 // prompt to ask for name when they visit the page
 // store than name in a variable
-// let userName = prompt("Hi! Whats your name?");
+let userName = prompt("Hi! Whats your name?");
 
-// $('.userName').text(userName);
+$('.userName').text(userName);
 
 
 // create a counter for the stars
 let counter = 0;
+
+const checkGoal = function() {
+    if (counter === 3) {
+        $(".levelOneButton").show();
+    }
+    if (counter === 6) {
+        $(".levelTwoButton").show();
+    }
+    if (counter === 9) {
+        $(".levelThreeButton").show();
+    }
+}
 
 $(".starButton").one('click', function() {
 
@@ -18,7 +30,7 @@ $(".starButton").one('click', function() {
     $('span.counter').text(counter);
     //fade out when clicked
     $(this).fadeOut();
-    // checkGoal();
+    checkGoal();
 })
 
 let lvlCounter = 1;
@@ -30,7 +42,6 @@ $(".levelOneButton").on('click', function() {
     $("body,html").animate({
         scrollTop: $("#levelTwo").offset().top
     }, 800);
-    // $(".levelOne").hide();
 })
 
 $(".levelTwoButton").on('click', function() {
@@ -39,6 +50,14 @@ $(".levelTwoButton").on('click', function() {
     $('span.lvlCounter').text(lvlCounter);
     $("body,html").animate({
         scrollTop: $("#levelThree").offset().top
+    }, 800);
+})
+
+$(".levelThreeButton").on('click', function() {
+    $(".credits").show();
+    $(".lvlTitle").hide();
+    $("body,html").animate({
+        scrollTop: $("#credits").offset().top
     }, 800);
 })
 
@@ -57,7 +76,10 @@ $(".levelTwoButton").on('click', function() {
 // Document Ready
 $(document).ready(function() {
     // starApp.init();
-    // $(".levelTwo").hide();
-    // $(".levelThree").hide();
-    // $(".credits").hide();
+    $(".levelOneButton").hide();
+    $(".levelTwoButton").hide();
+    $(".levelThreeButton").hide();
+    $(".levelTwo").hide();
+    $(".levelThree").hide();
+    $(".credits").hide();
 });
