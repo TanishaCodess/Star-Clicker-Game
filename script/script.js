4c68b7d8f959c6fb13734647eb9d814e9774534f
Lezgo7 <tanishaminson@gmail.com>
2020-09-13 17:09:12 -0400

    desktop done

--- script/script.js
+++ script/script.js
@@ -3,10 +3,20 @@ const starApp = {};
 
 // prompt to ask for name when they visit the page
 // store than name in a variable
+//user that name in the text
 let userName = prompt("Hi! Whats your name?");
 
-// use that name in text throughout the game
-$('.userName').text(userName);
+const checkName = function() {
+    if (userName === "") {
+        let userName = "friend";
+    } else {
+        $('.userName').text(userName);
+    }
+}
+checkName();
+
+
+
 
 
 // create a counter for the stars
@@ -43,35 +53,33 @@ const lootBox = [];
 $(".cookieCatButton").one('click', function() {
     $(this).fadeOut();
     $(".inventoryContainerOne").append(`<img src="Assets/icons/cookieCat.png" alt="Cookie Cat, an icecream sandwich that looks like a cat" width="50" height="50">`);
-    lootBox.push(`<img src="Assets/icons/cookieCat.png" alt="Cookie Cat, an icecream sandwich that looks like a cat" width="100" height="100"></img>`);
+    lootBox.push(`<img src="Assets/icons/cookieCat.png" alt="Cookie Cat, an icecream sandwich that looks like a cat"></img> <p>Remember, the key to Steven's Powers wasnt an icecream cake. It was the friends he made along the way.</p>`);
 })
 
 $(".pearlPointButton").one('click', function() {
     $(this).fadeOut();
     $(".inventoryContainerTwo").append(`<img src="Assets/icons/pearlPoint.png" alt="Pearl point sticker" width="50" height="50">`);
-    lootBox.push(`<img src="Assets/icons/pearlPoint.png" alt="Pearl point sticker" width="100" height="100">`);
+    lootBox.push(`<img src="Assets/icons/pearlPoint.png" alt="Pearl point sticker"> <p>Pearl thinks you did such a great job so she gave you a Pearl Point!</p>`);
 })
 
 $(".sheildButton").one('click', function() {
     $(this).fadeOut();
     $(".inventoryContainerThree").append(`<img src="Assets/icons/stevenShield.png" alt="Steven's shield" width="50" height="50">`);
-    lootBox.push(`<img src="Assets/icons/stevenShield.png" alt="Steven's shield" width="100" height="100">`);
+    lootBox.push(`<img src="Assets/icons/stevenShield.png" alt="Steven's shield"> <p>Steven's powers manifested into a shield so that he could protect the ones he cares about. We should all be more like Steven.</p>`);
 })
 
 //Loot Box
 $(".lootBoxButton").one('click', function() {
     $(this).fadeOut();
+    $(".lootSteven").hide();
     const randomLoot = lootBox[Math.floor(Math.random() * lootBox.length)];
     $(".lootDisplay").append(randomLoot);
+    $(".lootScreenButton").show();
+
 })
 
 
 
-function getLoot() {
-    lootBox[Math.floor(Math.random() * lootBox.length)];
-    console.log(getLoot);
-}
-getLoot();
 
 
 
@@ -110,6 +118,7 @@ $(".levelThreeButton").on('click', function() {
 $(".lootScreenButton").on('click', function() {
     $(".credits").show();
     $(".starCounter").hide();
+    $(".inventory").hide();
     $("body,html").animate({
         scrollTop: $("#credits").offset().top
     }, 800);
@@ -128,12 +137,12 @@ $(".lootScreenButton").on('click', function() {
 // Document Ready
 $(document).ready(function() {
     // starApp.init();
-    // $(".levelOneButton").hide();
-    // $(".levelTwoButton").hide();
-    // $(".levelThreeButton").hide();
-    // $(".lootScreenButton").hide();
-    // $(".levelTwo").hide();
-    // $(".levelThree").hide();
-    // $(".lootScreen").hide();
-    // $(".credits").hide();
+    $(".levelOneButton").hide();
+    $(".levelTwoButton").hide();
+    $(".levelThreeButton").hide();
+    $(".lootScreenButton").hide();
+    $(".levelTwo").hide();
+    $(".levelThree").hide();
+    $(".lootScreen").hide();
+    $(".credits").hide();
 });
