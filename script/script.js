64527e2d6d79a4a745262856ecaa9c3cfd200df3
Lezgo7 <tanishaminson@gmail.com>
2020-09-13 14:39:13 -0400

    added inventory items and functionality

--- script/script.js
+++ script/script.js
@@ -5,12 +5,15 @@ const starApp = {};
 // store than name in a variable
 let userName = prompt("Hi! Whats your name?");
 
+// use that name in text throughout the game
 $('.userName').text(userName);
 
 
 // create a counter for the stars
 let counter = 0;
 
+//check star counter 
+// reveal buttons as stars are found
 const checkGoal = function() {
     if (counter === 3) {
         $(".levelOneButton").show();
@@ -23,6 +26,7 @@ const checkGoal = function() {
     }
 }
 
+//stars
 $(".starButton").one('click', function() {
 
     // increase the counter value by 1
@@ -33,10 +37,43 @@ $(".starButton").one('click', function() {
     checkGoal();
 })
 
+//Inventory Items
+const lootBox = [];
+
+$(".cookieCatButton").one('click', function() {
+    $(this).fadeOut();
+    $(".inventoryContainerOne").append(`<img src="Assets/icons/cookieCat.png" alt="Cookie Cat, an icecream sandwich that looks like a cat" width="50" height="50">`);
+    lootBox.push(`<img src="Assets/icons/cookieCat.png" alt="Cookie Cat, an icecream sandwich that looks like a cat" width="50" height="50"></img>`);
+})
+
+$(".pearlPointButton").one('click', function() {
+    $(this).fadeOut();
+    $(".inventoryContainerTwo").append(`<img src="Assets/icons/pearlPoint.png" alt="Pearl point sticker" width="50" height="50">`);
+    lootBox.push(`<img src="Assets/icons/pearlPoint.png" alt="Pearl point sticker" width="50" height="50">`);
+})
+
+$(".sheildButton").one('click', function() {
+    $(this).fadeOut();
+    $(".inventoryContainerThree").append(`<img src="Assets/icons/stevenShield.png" alt="Steven's shield" width="50" height="50">`);
+    lootBox.push(`<img src="Assets/icons/stevenShield.png" alt="Steven's shield" width="50" height="50">`);
+})
+
+function getLoot() {
+    lootBox[Math.floor(Math.random() * lootBox.length)];
+    console.log(getLoot);
+}
+getLoot();
+
+
+
+//level counter in top left of screen
 let lvlCounter = 1;
+
+//on click, unhide next level and scroll to it
 $(".levelOneButton").on('click', function() {
 
     $(".levelTwo").show();
+    //increase level counter so user can see updated level title
     lvlCounter++;
     $('span.lvlCounter').text(lvlCounter);
     $("body,html").animate({
